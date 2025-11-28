974c3053fb2224239d7e33945ca0fb5ec1b522ad
fangxuling <fxl@xydata.cn>
2025-11-28 14:45:20 +0800

    chore: codeInspectorPlugin 工具定位代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 export default defineConfig({
-  base: '/', // Vercel 部署使用根路径
+  base: "/", // Vercel 部署使用根路径
   server: {
     proxy: {
-      '/api': {
-        target: 'https://spark-api-open.xf-yun.com',
+      "/api": {
+        target: "https://spark-api-open.xf-yun.com",
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-  plugins: [react()],
+  plugins: [
+    codeInspectorPlugin({
+      bundler: "vite",
+      hotKeys: ["altKey"],
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
+      "@": path.resolve(__dirname, "."),
+    },
   },
 });
